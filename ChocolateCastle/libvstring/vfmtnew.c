379ec32a6419d8fd114151fb7c573617641d3a0d
Thore Böckelmann <tboeckel@gmx.de>
2017-02-16 21:13:41 +0100

    * libvstring/vfmtnew.c: reverted the last change.

--- ChocolateCastle/libvstring/vfmtnew.c
+++ ChocolateCastle/libvstring/vfmtnew.c
@@ -7,15 +7,13 @@
 #include <exec/rawfmt.h>
 #endif
 
-#include <SDI_stdarg.h>
-
 STRPTR VFmtNew(CONST_STRPTR fmt, va_list args)
 {
 	ULONG l = 0;
 	STRPTR s;
-	VA_LIST copy;
+	va_list copy;
 
-	VA_COPY(copy, args);
+	va_copy(copy, args);
 
 	VNewRawDoFmt(fmt, RAWFMTFUNC_COUNT, (STRPTR)&l, args);
 
